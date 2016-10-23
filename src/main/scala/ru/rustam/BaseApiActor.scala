9965398d7c14fff210dc3df629291ee6c6554c8c
Rustam <rustamiksanov@gmail.com>
2016-10-23 22:37:48 +0300

    new Json and new table

--- src/main/scala/ru/rustam/BaseApiActor.scala
+++ src/main/scala/ru/rustam/BaseApiActor.scala
@@ -29,7 +29,63 @@ trait MyService extends HttpService {
 //        val body = ctx.request.entity.asString
         implicit val formats = DefaultFormats
         val body = read[WotData](ctx.request.entity.asString)
-        Db.con.sendQuery(s"INSERT INTO stat (json) VALUES ('$body');").onComplete {
+//        Db.con.sendQuery(s"INSERT INTO stat (json) VALUES ('$body');").onComplete {
+          Db.con.sendQuery(s"INSERT INTO stats(" +
+            s"xp, " +
+            s"cap, " +
+            s"defBase, " +
+            s"map, " +
+            s"frag, " +
+            s"gold, " +
+            s"hits, " +
+            s"spot, " +
+            s"tier, " +
+            s"idNum, " +
+            s"place, " +
+            s"shots, " +
+            s"assist, " +
+            s"damage, " +
+            s"freeXP, " +
+            s"result, " +
+            s"credits, " +
+            s"pierced, " +
+            s"vehicle, " +
+            s"battleTier, " +
+            s"battleTime, " +
+            s"battleType, " +
+            s"originalXP, " +
+            s"playerName, " +
+            s"assistRadio, " +
+            s"assistTrack, " +
+            s"playerAccount" +
+            s") VALUES (" +
+            s"'${body.xp}', " +
+            s"'${body.cap}'," +
+            s"'${body.defBase}'," +
+            s"'${body.map}'," +
+            s"'${body.frag}', " +
+            s"'${body.gold}' , " +
+            s"'${body.hits}' , " +
+            s"'${body.spot}' , " +
+            s"'${body.tier}' ," +
+            s"'${body.idNum}' ," +
+            s"'${body.place}' ," +
+            s"'${body.shots}' , " +
+            s"'${body.assist}' , " +
+            s"'${body.damage}' , " +
+            s"'${body.freeXP}' , " +
+            s"'${body.result}' , " +
+            s"'${body.credits}' , " +
+            s"'${body.pierced}' , " +
+            s"'${body.vehicle}' , " +
+            s"'${body.battleTier}' , " +
+            s"'${body.battleTime}' , " +
+            s"'${body.battleType}' , " +
+            s"'${body.originalXP}' , " +
+            s"'${body.playerName}' , " +
+            s"'${body.assistRadio}' , " +
+            s"'${body.assistTrack}' , " +
+            s"'${body.playerAccount}' );").onComplete {
           case Success(res) =>
             ctx.complete(s"Got $body and saved to db.")
           case Failure(err) =>
